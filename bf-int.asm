a0f9f0c26df00cd693c0919772d70e400d4047cc
Book-reader <thevoid@outlook.co.nz>
2024-09-15 19:35:40 +1200

    it was the "cmp dword [curr_cell], 0" but the cell is only a byte :(. I cant belive I spent 4 hours on this

--- bf-int.asm
+++ bf-int.asm
@@ -1,12 +1,10 @@
 section .data
 	; Null terminated string to show end of program
 	; This is 'Hello, World!'
-	; prog db "++++++++[>+++++++++>+++++++++++++>++++++>++++>+++++++++++<<<<<-]>.>---.+++++++..+++.>----.>.>-.<<<.+++.------.--------.>>+.", 0
+	prog db "++++++++[>+++++++++>+++++++++++++>++++++>++++>+++++++++++<<<<<-]>.>---.+++++++..+++.>----.>.>-.<<<.+++.------.--------.>>+.", 0
 	; prog db "+++++++++++++++++++++++++++++++++.+.>+++++++++-+++++++++++++++++++++++++++.", 0
-	prog db "[>+<-]>.", 0
+	; prog db "+[>+<-]>.", 0
 	prog_idx dd 0
-	; The current inst
-	curr_inst dd 0
 	; The 30kib of program memory
 	prog_mem times 30000 db 0
 	; The current memory index
@@ -27,12 +25,6 @@ loop:
 	; Load the current program instruction into eax
 	mov eax, prog
 	add eax, [prog_idx]
-	; TODO just use eax instead of curr_inst, it doesnt need to be a variable
-	; mov ebx, [eax]
-	; mov [curr_inst], ebx
-
-	; Print the current instruction
-	; call printc
 
 	; Load the current cell at prog_ptr to ebx
 	call load_cell
@@ -89,19 +81,13 @@ loop:
 	jmp .fin
 .inst_loop_f:
 	mov ebx, 1
-	cmp dword [curr_cell], 0
+	cmp byte [curr_cell], 0
 	je .goto_next_loop_f
 	jmp .fin
 .goto_next_loop_f:
-	; mov eax, tmp
-	; mov word [eax], '['
-	; mov ebx, eax
-	; mov edx, ebx
-	; call printc
-	inc dword [prog_idx]
 	cmp ebx, 0
 	je .fin
-	
+	inc dword [prog_idx]
 	mov eax, prog
 	add eax, [prog_idx]
 	cmp byte [eax], ']'
@@ -118,21 +104,14 @@ loop:
 
 .inst_loop_b:
 	mov ebx, 1
-	cmp dword [curr_cell], 0
+	cmp byte [curr_cell], 0
 	jne .goto_prev_loop_b
 	jmp .fin
 
 .goto_prev_loop_b:
-	mov eax, tmp
-	mov word [eax], ']'
-	mov ebx, eax
-	mov edx, ebx
-	call printc
-
-	dec dword [prog_idx]
-	cmp ebx, 0
+	cmp dword ebx, 0
 	je .fin
-
+	dec dword [prog_idx]
 	mov eax, prog
 	add eax, [prog_idx]
 	cmp byte [eax], ']'
@@ -148,21 +127,11 @@ loop:
 	jmp .goto_prev_loop_b
 
 .fin:
-	mov eax, prog
-	add eax, [prog_idx]
-	mov edx, eax
-	call printc
-	; mov eax, tmp
-	; add eax, 0
-	; mov dword [eax], prog_idx
-	; mov edx, eax
-	; call printc
-	; Increment the prog_idx
 	inc dword [prog_idx]
 	mov eax, prog
 	add eax, [prog_idx]
 	; Check if the current instruction is 0, the end of the program
-	cmp word [eax], 0
+	cmp byte [eax], 0
 	; If it isnt, repeat the loop
 	jne loop
 	; If it is, break
